bc1edabc11b7d88ff7ed93f35b062e27b976afcb
coolpeter98 <103730479+coolpeter98@users.noreply.github.com>
2023-06-10 23:52:56 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
+echo "hi~~💕 i'm installing Co!"
 cd /Applications/Roblox.app/Contents/MacOS
-if [ ! -f RCOPATCHED ]; then
+if [ ! -f COPATCHED ]; then
 mv RobloxPlayer ActualRobloxPlayer
 echo "#!/bin/sh" >> RobloxPlayer
 echo "rm -rf /Applications/Roblox.app/Contents/MacOS/ClientSettings && mkdir /Applications/Roblox.app/Contents/MacOS/ClientSettings && curl https://raw.githubusercontent.com/coolpeter98/Rco/main/ClientAppSettings.json -o /Applications/Roblox.app/Contents/MacOS/ClientSettings/ClientAppSettings.json && exec /Applications/Roblox.app/Contents/MacOS/ActualRobloxPlayer" >> RobloxPlayer
 chmod +x RobloxPlayer
-touch RCOPATCHED
-echo "Finished installing RCO"
+touch COPATCHED
+echo "hiiii cutie >w< i have finished installing Co!"
 else
-echo "You have already installed RCO!"
+echo "hey sweetie💖~ you already installed Co!"
 fi
